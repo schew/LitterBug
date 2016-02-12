04831073eb3e808b411c166c45c263d8ab2889f1
Elana Stroud <estroud1@jhu.edu>
2016-02-12 14:20:30 -0500

    bower actually works with bower components, tested with jquery

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.use('/', routes);
 app.use('/users', users);
 
---
+++ public/javascripts/bargraph.js
@@ -0,0 +1,9 @@
+/**
+ * Created by elanastroud on 2/12/16.
+ */
+
+console.log("test")
+
+$(document).ready(function(){
+    $("#test").css("background", "blue")
+})
