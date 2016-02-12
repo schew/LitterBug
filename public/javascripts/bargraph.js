b0494ef470d470307a5a63b424323ffd64d104d6
Elana Stroud <estroud1@jhu.edu>
2016-02-12 14:50:57 -0500

    trying to get epoch graphs to show up

--- public/javascripts/bargraph.js
+++ public/javascripts/bargraph.js
@@ -4,6 +4,37 @@
 
 console.log("test")
 
-$(document).ready(function(){
-    $("#test").css("background", "blue")
-})
+var data = [
+    { label: 'Layer 1', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 2} ] },
+    { label: 'Layer 2', values: [ {x: 0, y: 0}, {x: 1, y: 1}, {x: 2, y: 4} ] }
+];
+//
+//var areaChartInstance = $('#area').epoch({
+//    type: 'area',
+//    data: data,
+//    axes: ['left', 'right', 'bottom']
+//});
+
+var barChartData = [
+    // First bar series
+    {
+        label: 'Series 1',
+        values: [
+            { x: 'A', y: 30 },
+            { x: 'B', y: 10 },
+                { x: 'C', y: 12 }]
+    },
+
+    // Second series
+    {
+        label: 'Series 2',
+            values: [
+        { x: 'A', y: 20 },
+        { x: 'B', y: 39 },
+        { x: 'C', y: 8 }]
+}];
+
+$('#barChart').epoch({
+    type: 'bar',
+    data: barChartData
+});
