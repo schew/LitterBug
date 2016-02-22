fce4bb677ccd179d8e5547099833d2ed153e8038
schew4 <schew4@jhu.edu>
2016-02-21 20:54:05 -0500

    added changes to charts

--- public/javascripts/piechart.js
+++ public/javascripts/piechart.js
@@ -1,13 +1,4 @@
-// /* Pie charts consist of solid slices where you can use this selector to override the default style. */
-// .ct-series-a .ct-slice-pie {
-//   /* fill of the pie slieces */
-//   fill: hsl(120, 40%, 60%);
-//   /* give your pie slices some outline or separate them visually by using the background color here */
-//   stroke: white;
-//   /* outline width */
-//   stroke-width: 4px;
-// }
-
+// waste composition
 var data = {
 	labels: ['Food', 'Plastic', 'Potential Recyclables', 'Metal'],
 	series: ['10', '20', '5', '15']
@@ -33,4 +24,34 @@ var responsiveOptions = [
   }]
 ];
 
-new Chartist.Pie('.pie-chart', data, options, responsiveOptions);
+new Chartist.Pie('.waste-comp', data, options, responsiveOptions);
+
+
+
+//breakdown of waste reduction
+var data = {
+	labels: ['Food', 'Plastic', 'Potential Recyclables', 'Metal'],
+	series: ['5', '1', '8', '3']
+};
+
+var options = {
+	width: '600px',
+	height: '600px',
+};
+
+var responsiveOptions = [
+  ['screen and (min-width: 640px)', {
+    chartPadding: 30,
+    labelOffset: 100,
+    labelDirection: 'explode',
+    labelInterpolationFnc: function(value) {
+      return value;
+    }
+  }],
+  ['screen and (min-width: 1024px)', {
+    labelOffset: 100,
+    chartPadding: 50
+  }]
+];
+
+new Chartist.Pie('.waste-reduction', data, options, responsiveOptions);
