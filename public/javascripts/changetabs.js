e288f48a91c9b413298a5fc90c0a930d5ca0d237
Elana Stroud <estroud1@jhu.edu>
2016-03-27 16:38:46 -0400

    added challenge button

--- public/javascripts/changetabs.js
+++ public/javascripts/changetabs.js
@@ -23,13 +23,11 @@ function showSettings() {
 */
 
 function swap(activate, deactivate) {
-	$(deactivate[0]).css("display", "none");
-	$(activate[0]).css("display", "inline-block");
+
+    $(deactivate[0]).css("display", "none");
+    $(activate[0]).css("display", "inline-block");
     $(deactivate[1]).css("background", "none");
     $(activate[1]).css("background", "rgb(205, 0, 0)");
 
-    console.log(activate)
-    console.log(deactivate)
-
-	activeTab=activate;
+    activeTab=activate;
 }
