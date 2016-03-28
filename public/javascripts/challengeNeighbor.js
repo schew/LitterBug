b47dd44a67f0a9e61506862cdfbc8f48fbf25751
Elana Stroud <estroud1@jhu.edu>
2016-03-27 22:35:12 -0400

    formatting

--- public/javascripts/challengeNeighbor.js
+++ public/javascripts/challengeNeighbor.js
@@ -31,7 +31,7 @@ function showModal() {
         }
     );
 
-    $(".sa-button-container").css("margin-left", "-70px")
+//    $(".sa-button-container").css("margin-left", "-70px")
     $(".sa-confirm-button-container").css("margin-bottom", "30px")
     $(".showSweetAlert").css("background", "rgb(255,255,255")
     $(".showSweetAlert").css("opacity", "0.9")
