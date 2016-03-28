f2bd31f84b8bac8ffb358b4b4fac8929b28e132e
schew4 <schew4@jhu.edu>
2016-03-27 20:18:07 -0400

    Fixed background

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -1,11 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) {
 	$scope.reduce = function(weight) {
 		// Error check -- can't have negative weight
-		if (weight < 0) {
+		if (weight < 0 || typeof weight == 'undefined') {
 			weight = 0;
-		} else if (typeof weight == 'undefined') {
-			// TODO: HARDCODED NUMBERS
-			weight = 56;
 		}
 
 		return 56 - weight;
@@ -15,7 +12,8 @@ app.controller('MainController', ['$scope', function($scope) {
 		if (typeof newWeight == 'undefined') {
 			newWeight = 0;
 		}
-		var savings = newWeight * 0.45
+		var savings = newWeight * 0.45;
+		
 		return savings.toFixed(2);
 	}
 }]);
